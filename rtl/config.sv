e836cb49f91aed7c0256f70309a6a85607826be1
Jack Chen <redchenjs@live.com>
2021-06-06 13:46:52 +0800

    rtl: 100 MHz / 6 Phases

--- rtl/config.sv
+++ rtl/config.sv
@@ -5,7 +5,4 @@
  *      Author: Jack Chen <redchenjs@live.com>
  */
 
-parameter [3:0] RTL_REVISION_MAJOR = 4'h1;
-parameter [3:0] RTL_REVISION_MINOR = 4'h0;
-
-parameter [32:0] DEFAULT_GATE_TIME = 32'd49_999;
+parameter [32:0] DEFAULT_GATE_TIME = 32'd24_999;
--- rtl/measure.sv
+++ rtl/measure.sv
@@ -20,37 +20,45 @@ module measure(
     output logic [63:0] reg_wr_data_o
 );
 
-logic clk_cnt_rst;
-
 logic [5:0] sig_clk_p;
+logic [5:0] clk_cnt_rst;
 
 logic        gate_en;
 logic [31:0] gate_cnt;
 
 logic [5:0] gate_sync;
-logic       gate_sync_n;
+logic [5:0] gate_sync_n;
+
+logic       gate_sync_rst;
+logic [5:0] gate_sync_done;
+logic       gate_sync_done_p;
 
 logic [31:0] sig_clk_cnt[6];
 logic [31:0] ref_clk_cnt[6];
 
+logic [31:0] sig_clk_tmp[6];
+logic [31:0] ref_clk_tmp[6];
+
+logic [31:0] sig_clk_syn[6];
+logic [31:0] ref_clk_syn[6];
+
+logic [31:0] sig_clk_out[6];
+logic [31:0] ref_clk_out[6];
+
+logic [31:0] sig_clk_sum;
+logic [31:0] ref_clk_sum;
+
+logic  [3:0] reg_wr_en;
 logic [63:0] reg_wr_data;
 
-wire gate_sync_or = gate_sync[0] | gate_sync[1] | gate_sync[2] |
-                    gate_sync[3] | gate_sync[4] | gate_sync[5];
-
-wire [31:0] sig_clk_out = sig_clk_cnt[0] + sig_clk_cnt[1] + sig_clk_cnt[2] +
-                          sig_clk_cnt[3] + sig_clk_cnt[4] + sig_clk_cnt[5];
-wire [31:0] ref_clk_out = ref_clk_cnt[0] + ref_clk_cnt[1] + ref_clk_cnt[2] +
-                          ref_clk_cnt[3] + ref_clk_cnt[4] + ref_clk_cnt[5];
-
-assign reg_wr_en_o   = gate_sync_n;
+assign reg_wr_en_o   = reg_wr_en[3];
 assign reg_wr_data_o = reg_wr_data;
 
-edge2en gate_sync_en(
+edge2en gate_sync_done_en(
     .clk_i(clk_i),
     .rst_n_i(rst_n_i),
-    .data_i(gate_sync_or),
-    .neg_edge_o(gate_sync_n)
+    .data_i(gate_sync_done == 6'h3f),
+    .pos_edge_o(gate_sync_done_p)
 );
 
 genvar i;
@@ -63,6 +71,13 @@ generate
             .pos_edge_o(sig_clk_p[i])
         );
 
+        edge2en gate_sync_en(
+            .clk_i(clk_i),
+            .rst_n_i(rst_n_i),
+            .data_i(gate_sync[i]),
+            .neg_edge_o(gate_sync_n[i])
+        );
+
         always_ff @(posedge ref_clk_i[i] or negedge ref_rst_n_i[i])
         begin
             if (!ref_rst_n_i[i]) begin
@@ -70,19 +85,26 @@ generate
 
                 sig_clk_cnt[i] <= 32'h0000_0000;
                 ref_clk_cnt[i] <= 32'h0000_0000;
+
+                sig_clk_tmp[i] <= 32'h0000_0000;
+                ref_clk_tmp[i] <= 32'h0000_0000;
             end else begin
                 if (gate_sync[i]) begin
                     gate_sync[i] <= sig_clk_p[i] & ~gate_en ? 1'b0 : gate_sync[i];
-                end else begin
-                    gate_sync[i] <= sig_clk_p[i] & gate_en ? 1'b1 : gate_sync[i];
-                end
 
-                if (clk_cnt_rst) begin
-                    sig_clk_cnt[i] <= 32'h0000_0000;
-                    ref_clk_cnt[i] <= 32'h0000_0000;
-                end else if (gate_sync[i]) begin
                     sig_clk_cnt[i] <= sig_clk_p[i] ? sig_clk_cnt[i] + 1'b1 : sig_clk_cnt[i];
                     ref_clk_cnt[i] <= ref_clk_cnt[i] + 1'b1;
+
+                    sig_clk_tmp[i] <= 32'h0000_0000;
+                    ref_clk_tmp[i] <= 32'h0000_0000;
+                end else begin
+                    gate_sync[i] <= sig_clk_p[i] & gate_en ? 1'b1 : gate_sync[i];
+
+                    sig_clk_cnt[i] <= 32'h0000_0000;
+                    ref_clk_cnt[i] <= 32'h0000_0000;
+
+                    sig_clk_tmp[i] <= sig_clk_cnt[i];
+                    ref_clk_tmp[i] <= ref_clk_cnt[i];
                 end
             end
         end
@@ -95,16 +117,50 @@ begin
         gate_en  <= 1'b0;
         gate_cnt <= 32'h0000_0000;
 
-        clk_cnt_rst <= 1'b0;
+        gate_sync_rst  <= 1'b1;
+        gate_sync_done <= 6'h00;
 
+        for (integer i = 0; i < 6; i++) begin
+            sig_clk_syn[i] <= 32'h0000_0000;
+            ref_clk_syn[i] <= 32'h0000_0000;
+
+            sig_clk_out[i] <= 32'h0000_0000;
+            ref_clk_out[i] <= 32'h0000_0000;
+        end
+
+        sig_clk_sum <= 32'h0000_0000;
+        ref_clk_sum <= 32'h0000_0000;
+
+        reg_wr_en   <= 4'h0;
         reg_wr_data <= 64'h0000_0000_0000_0000;
     end else begin
-        gate_en  <= gate_sync_or ? ((gate_cnt == DEFAULT_GATE_TIME) ? 1'b0 : gate_en) : 1'b1;
-        gate_cnt <= gate_en & gate_sync_or & (gate_cnt != DEFAULT_GATE_TIME) ? gate_cnt + 1'b1 : 32'h0000_0000;
+        gate_en  <= ~gate_sync_rst ? ((gate_cnt == DEFAULT_GATE_TIME) ? 1'b0 : gate_en) : 1'b1;
+        gate_cnt <= ~gate_sync_rst & gate_en & (gate_cnt != DEFAULT_GATE_TIME) ? gate_cnt + 1'b1 : 32'h0000_0000;
 
-        clk_cnt_rst <= gate_sync_n;
+        gate_sync_rst <= reg_wr_en[3];
 
-        reg_wr_data <= gate_sync_n ? {ref_clk_out, sig_clk_out} : reg_wr_data;
+        for (integer i = 0; i < 6; i++) begin
+            gate_sync_done[i] <= gate_sync_rst ? 1'b0 : (gate_sync_n[i] ? 1'b1 : gate_sync_done[i]);
+        end
+
+        for (integer i = 0; i < 6; i++) begin
+            sig_clk_syn[i] <= gate_sync_n[i] ? sig_clk_tmp[i] : sig_clk_syn[i];
+            ref_clk_syn[i] <= gate_sync_n[i] ? ref_clk_tmp[i] : ref_clk_syn[i];
+
+            sig_clk_out[i] <= gate_sync_n[i] ? sig_clk_syn[i] : sig_clk_out[i];
+            ref_clk_out[i] <= gate_sync_n[i] ? ref_clk_syn[i] : ref_clk_out[i];
+        end
+
+        sig_clk_sum <= reg_wr_en[0] ? sig_clk_out[0] + sig_clk_out[1] + sig_clk_out[2] :
+                       reg_wr_en[1] ? sig_clk_out[3] + sig_clk_out[4] + sig_clk_out[5] + sig_clk_sum :
+                       sig_clk_sum;
+
+        ref_clk_sum <= reg_wr_en[0] ? ref_clk_out[0] + ref_clk_out[1] + ref_clk_out[2] :
+                       reg_wr_en[1] ? ref_clk_out[3] + ref_clk_out[4] + ref_clk_out[5] + ref_clk_sum :
+                       ref_clk_sum;
+
+        reg_wr_en   <= {reg_wr_en[2:0], gate_sync_done_p};
+        reg_wr_data <= reg_wr_en[2] ? {ref_clk_sum, sig_clk_sum} : reg_wr_data;
     end
 end
 
