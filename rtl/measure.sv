3dc5eb1165467ef2d7f8e6f5a0d0e83dc3da0492
Jack Chen <redchenjs@live.com>
2021-06-17 21:36:10 +0800

    rtl: WIP update

--- rtl/measure.sv
+++ rtl/measure.sv
@@ -20,24 +20,24 @@ module measure(
     output logic [63:0] reg_wr_data_o
 );
 
-logic [3:0] sig_clk_p;
+logic sig_rst_n;
+
+logic gate_real;
+logic gate_real_n;
+
+logic sig_gate;
+logic sig_gate_n;
+
+logic [3:0] ref_gate;
+logic [3:0] ref_gate_n;
 
 logic        gate_en;
 logic [31:0] gate_cnt;
 
-logic [3:0] gate_sync;
-logic [3:0] gate_sync_p;
-logic [3:0] gate_sync_n;
+logic [31:0] sig_clk_cnt;
+logic [31:0] sig_clk_out;
 
-logic       gate_sync_rst;
-logic [3:0] gate_sync_init;
-logic [3:0] gate_sync_done;
-logic       gate_sync_done_p;
-
-logic [31:0] sig_clk_cnt[4];
 logic [31:0] ref_clk_cnt[4];
-
-logic [31:0] sig_clk_out[4];
 logic [31:0] ref_clk_out[4];
 
 logic [31:0] sig_clk_sum;
@@ -49,50 +49,76 @@ logic [63:0] reg_wr_data;
 assign reg_wr_en_o   = reg_wr_en[3];
 assign reg_wr_data_o = reg_wr_data;
 
-edge2en gate_sync_done_en(
+rst_syn sig_rst_syn(
+    .clk_i(sig_clk_i),
+    .rst_n_i(rst_n_i),
+    .rst_n_o(sig_rst_n)
+);
+
+data_syn sig_gate_syn(
+    .clk_i(sig_clk_i),
+    .rst_n_i(sig_rst_n),
+    .data_i(gate_en),
+    .data_o(sig_gate)
+);
+
+edge2en sig_gate_en(
+    .clk_i(sig_clk_i),
+    .rst_n_i(sig_rst_n),
+    .data_i(sig_gate),
+    .neg_edge_o(sig_gate_n)
+);
+
+data_syn gate_real_syn(
     .clk_i(clk_i),
     .rst_n_i(rst_n_i),
-    .data_i(gate_sync_done == 4'hf),
-    .pos_edge_o(gate_sync_done_p)
+    .data_i(sig_gate),
+    .data_o(gate_real)
 );
+
+edge2en gate_real_en(
+    .clk_i(clk_i),
+    .rst_n_i(rst_n_i),
+    .data_i(gate_real),
+    .neg_edge_o(gate_real_n)
+);
+
+always_ff @(posedge sig_clk_i or negedge sig_rst_n)
+begin
+    if (!sig_rst_n) begin
+        sig_clk_cnt <= 32'h0000_0000;
+        sig_clk_out <= 32'h0000_0000;
+    end else begin
+        sig_clk_cnt <= sig_gate_n ? 32'h0000_0000 : (sig_gate ? sig_clk_cnt + 1'b1 : sig_clk_cnt);
+        sig_clk_out <= sig_gate_n ? sig_clk_cnt : sig_clk_out;
+    end
+end
 
 genvar i;
 generate
     for (i = 0; i < 4; i++) begin: measure_block
-        edge2en sig_clk_en(
+        data_syn ref_gate_syn(
             .clk_i(ref_clk_i[i]),
             .rst_n_i(ref_rst_n_i[i]),
-            .data_i(sig_clk_i),
-            .pos_edge_o(sig_clk_p[i])
+            .data_i(sig_gate),
+            .data_o(ref_gate[i])
         );
 
-        edge2en gate_sync_en(
-            .clk_i(clk_i),
-            .rst_n_i(rst_n_i),
-            .data_i(gate_sync[i]),
-            .pos_edge_o(gate_sync_p[i]),
-            .neg_edge_o(gate_sync_n[i])
+        edge2en ref_gate_en(
+            .clk_i(ref_clk_i[i]),
+            .rst_n_i(ref_rst_n_i[i]),
+            .data_i(ref_gate[i]),
+            .neg_edge_o(ref_gate_n[i])
         );
 
         always_ff @(posedge ref_clk_i[i] or negedge ref_rst_n_i[i])
         begin
             if (!ref_rst_n_i[i]) begin
-                gate_sync[i] <= 1'b0;
-
-                sig_clk_cnt[i] <= 32'h0000_0000;
                 ref_clk_cnt[i] <= 32'h0000_0000;
+                ref_clk_out[i] <= 32'h0000_0000;
             end else begin
-                if (gate_sync[i]) begin
-                    gate_sync[i] <= sig_clk_p[i] & ~gate_en ? 1'b0 : gate_sync[i];
-
-                    sig_clk_cnt[i] <= sig_clk_p[i] ? sig_clk_cnt[i] + 1'b1 : sig_clk_cnt[i];
-                    ref_clk_cnt[i] <= ref_clk_cnt[i] + 1'b1;
-                end else begin
-                    gate_sync[i] <= sig_clk_p[i] & gate_en ? 1'b1 : gate_sync[i];
-
-                    sig_clk_cnt[i] <= sig_clk_p[i] ? 32'h0000_0000 : sig_clk_cnt[i];
-                    ref_clk_cnt[i] <= sig_clk_p[i] ? 32'h0000_0000 : ref_clk_cnt[i];
-                end
+                ref_clk_cnt[i] <= ref_gate_n[i] ? 32'h0000_0000 : (ref_gate[i] ? ref_clk_cnt[i] + 1'b1 : ref_clk_cnt[i]);
+                ref_clk_out[i] <= ref_gate_n[i] ? ref_clk_cnt[i] : ref_clk_out[i];
             end
         end
     end
@@ -104,45 +130,21 @@ begin
         gate_en  <= 1'b0;
         gate_cnt <= 32'h0000_0000;
 
-        gate_sync_rst  <= 1'b0;
-        gate_sync_init <= 4'h0;
-        gate_sync_done <= 4'h0;
-
-        for (integer i = 0; i < 4; i++) begin
-            sig_clk_out[i] <= 32'h0000_0000;
-            ref_clk_out[i] <= 32'h0000_0000;
-        end
-
         sig_clk_sum <= 32'h0000_0000;
         ref_clk_sum <= 32'h0000_0000;
 
         reg_wr_en   <= 4'h0;
         reg_wr_data <= 64'h0000_0000_0000_0000;
     end else begin
-        gate_en  <= (gate_sync_init == 4'hf) ? ((gate_cnt == DEFAULT_GATE_TIME) ? 1'b0 : gate_en) : 1'b1;
-        gate_cnt <= (gate_sync_init == 4'hf) & gate_en & (gate_cnt != DEFAULT_GATE_TIME) ? gate_cnt + 1'b1 : 32'h0000_0000;
+        gate_en  <= gate_real ? ((gate_cnt == DEFAULT_GATE_TIME) ? 1'b0 : gate_en) : 1'b1;
+        gate_cnt <= gate_real & gate_en & (gate_cnt != DEFAULT_GATE_TIME) ? gate_cnt + 1'b1 : 32'h0000_0000;
 
-        gate_sync_rst <= reg_wr_en[3];
-
-        for (integer i = 0; i < 4; i++) begin
-            gate_sync_init[i] <= gate_sync_rst ? 1'b0 : (gate_sync_p[i] ? 1'b1 : gate_sync_init[i]);
-            gate_sync_done[i] <= gate_sync_rst ? 1'b0 : (gate_sync_n[i] ? 1'b1 : gate_sync_done[i]);
-        end
-
-        for (integer i = 0; i < 4; i++) begin
-            sig_clk_out[i] <= (gate_sync_done == 4'hf) ? sig_clk_cnt[i] : sig_clk_out[i];
-            ref_clk_out[i] <= (gate_sync_done == 4'hf) ? ref_clk_cnt[i] : ref_clk_out[i];
-        end
-
-        sig_clk_sum <= reg_wr_en[0] ? sig_clk_out[0] + sig_clk_out[1] :
-                       reg_wr_en[1] ? sig_clk_out[2] + sig_clk_out[3] + sig_clk_sum :
-                       sig_clk_sum;
-
+        sig_clk_sum <= reg_wr_en[0] ? sig_clk_cnt : sig_clk_sum;
         ref_clk_sum <= reg_wr_en[0] ? ref_clk_out[0] + ref_clk_out[1] :
                        reg_wr_en[1] ? ref_clk_out[2] + ref_clk_out[3] + ref_clk_sum :
                        ref_clk_sum;
 
-        reg_wr_en   <= {reg_wr_en[2:0], gate_sync_done_p};
+        reg_wr_en   <= {reg_wr_en[2:0], gate_real_n};
         reg_wr_data <= reg_wr_en[2] ? {ref_clk_sum, sig_clk_sum} : reg_wr_data;
     end
 end
